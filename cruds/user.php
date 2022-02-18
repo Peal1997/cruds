515e90463a0cbff27c4c56893ff7be43f3f9bf78
Peal Hasan <52187350+Peal1997@users.noreply.github.com>
2022-02-18 15:36:03 +0600

    Add files via upload

---
+++ cruds/user.php
@@ -0,0 +1,117 @@
+<?php
+include_once "./autoloader.php";
+
+$delete_id = $_GET['delete_id'] ?? false;
+connect() -> query("DELETE from  teachers where id='$delete_id'  ");
+header('loction:user.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Teachers Info</title>
+	
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	
+</head>
+<body>
+	<div class="wrap-table  shadow">
+    <a class="btn btn-primary btn-sm" href="./index.php">add new teachers</a>
+        <br>
+        <br>
+		<div class="card">
+			<div class="card-body">
+ <form action="" method="POST" class="row gx-5 align-items-center">
+		<div class="col-auto">
+			<select name="gender" class="form-select" id="">
+			<option selected>Gender</option>
+			<option value="Male">Male</option>
+			<option value="Female">Female</option>
+			
+			</select>
+		</div>
+		
+		<div class="col-auto">
+			<select name="edu" class="form-select" id="">
+			<option selected>Education</option>
+			<option value="Bachelor">Bachelor</option>
+			<option value="Masters">Masters</option>
+			<option value="Phd">Phd</option>
+			<option value="PGDM">PGDM</option>
+			</select>
+		</div>
+		<div class="col-auto">
+			<input name="search" type="submit" class="btn btn-info" value="Search">
+
+		</div>
+ </form>
+          <h2>All Data</h2>
+				<table style="margin:20px" class="table table-striped">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Name</th>
+							<th>Salary</th>
+							<th>Email</th>
+							<th>Cell</th>
+							<th>Department</th>
+							<th>Gender</th>
+							<th>Education</th>
+							<th>photo</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+                 <?php
+                    $data = connect() -> query("SELECT  * from teachers" );
+					if(isset($_POST['search'])){
+						$gender = $_POST['gender'];
+						$education = $_POST['edu'];
+						$data = connect() -> query("SELECT  * from teachers WHERE gender = '$gender' AND education = '$education ' " );
+					}
+            
+                    $sn =1;
+                    while($user = $data -> fetch_object() ):
+                    ?>
+                        <tr>
+							<td><?php echo $sn++ ; ?></td>
+							<td><?php echo $user -> name; ?></td>  
+							<td><?php echo $user -> salary; ?></td>
+							<td><?php echo $user -> email ; ?></td>
+							<td><?php echo $user -> cell ; ?></td>
+							<td><?php echo $user -> department ; ?></td>
+							<td><?php echo $user -> gender; ?></td>
+							<td><?php echo $user -> education; ?></td>
+							<td><img style="width:100px" src="photos/<?php echo $user -> photo ; ?>" alt=""></td>
+							<td>
+								<a class="btn btn-sm btn-info" href="./single.php?user_id=<?php echo $user -> id; ?>">View</a>
+								<a class="btn btn-sm btn-warning" href="./edit.php?edit_id=<?php echo $user -> id; ?>">Edit</a>
+								<a class="btn btn-sm btn-danger delete_btn" href="?delete_id=<?php echo $user -> id; ?>">Delete</a>
+							</td>
+						</tr>
+                        <?php endwhile; ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<!-- JS FILES  -->
+	<script src ="assets/js/jquery-3.6.0.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	
+	<script src="assets/js/scripts.js"></script>
+	<script>
+		$('.delete_btn').click(function(){
+            
+          let conf =  confirm('Are you sure ??');
+		  if(conf){
+			  return true;
+		  }else{
+			  return false;
+		  }
+		});
+	</script>
+</body>
+</html>
